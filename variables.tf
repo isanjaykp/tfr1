fda308a70b46752f2faa86510ea6b8acb442cbb4
isanjaykp <isanjaykp@gmail.com>
2023-08-07 20:17:08 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-1"
+  default = "us-east-1"
 }
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-0f34c5ae932e6f0e4"
+  default = "ami-053b0d53c279acc90"
 }
 
 variable "instance_type" {
